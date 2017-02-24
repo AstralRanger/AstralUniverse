59e311957ec56f849427addb23dee39692eda045
Jerry Yang <z295yang@edu.uwaterloo.ca>
2017-02-23 23:14:15 -0500

    Added Objects
    - You can now buy strings for 50 matter
    - No use as of yet

--- programming.js
+++ programming.js
@@ -1,6 +1,13 @@
 var matter = 0;
 
 var action = 0;
+
+var objects = [0];
+
+var prices = [50];
+
+var object_types = ["String"];
+
 function showBolt() {
 	matter++;
 	document.getElementById("matter-amount").innerHTML = "Matter: " + matter;
@@ -8,5 +15,15 @@ function showBolt() {
 	action = setTimeout(function hideBolt() {
 						document.getElementById("bolt").style.display = "none";
 						}, 50);
+}
+
+function buy(type) {
+	if (matter >= prices[type])
+	{
+		matter -= prices[type];
+		objects[type]++;
+		prices[type] = Math.round(0.23*Math.pow(objects[type]+1, 3) + 3.02*Math.pow(objects[type]+1, 2) + 4.92*(objects[type]+1) + 42);
+		document.getElementById("item" + type).innerHTML = object_types[type] + " - " + prices[type] + " Matter, Owned: " + objects[type];
+	}
 }
 
